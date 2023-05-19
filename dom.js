15b1aa56b6c8a913a863421020f33d15c3cd1883
AmitkrMeena02 <amitmeena.meena@yahoo.com>
2023-05-19 16:43:00 +0530

    items

---
+++ dom.js
@@ -0,0 +1,6 @@
+let headerTitle = document.getElementById(`header-title`);
+let header = document.getElementById(`main-header`);
+header.style.borderBottom = `solid 3px #000`;
+let addTitle = document.querySelector(`.title`);
+addTitle.style.fontWeight = `bold`;
+addTitle.style.color = `green`;
